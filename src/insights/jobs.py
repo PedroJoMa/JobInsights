534900c52c86967822871cc84aebd4057fd7521d
PedroJoMa <pejomadejefi@gmail.com>
2023-05-01 14:18:20 -0300

    Função read retorna todos os dados no formato de uma lista de dicionários

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,22 +1,18 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
+    with open(path, encoding="utf-8") as file:
+        jobs_reader = csv.DictReader(file, delimiter=",", quotechar='"')
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
+        all_jobs = []
+        for job in jobs_reader:
+            all_jobs.append(job)
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    # raise NotImplementedError
+    return all_jobs
 
 
 def get_unique_job_types(path: str) -> List[str]:
