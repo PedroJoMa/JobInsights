589f0bf075f4ab2273dfcf97b8d24f468c5b9776
PedroJoMa <pejomadejefi@gmail.com>
2023-05-01 15:09:12 -0300

    Função get_unique_industries retorna uma lista de valores únicos presentes na coluna industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,19 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    all_jobs = read(path)
 
-    Must call `read`
+    all_industries = []
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
+    for industry in all_jobs:
+        row = industry["industry"]
+        if row not in all_industries:
+            if row != "":
+                all_industries.append(row)
 
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    return all_industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
