d4a4de41f29e3478f78e2dd850dfd4e2371f9258
PedroJoMa <pejomadejefi@gmail.com>
2023-05-03 21:20:10 -0300

    Requisito 10 - a terminar

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
+    path = "data/jobs.csv"
+    word = "Python"
+
+    assert count_ocurrences(path, word) == 1639
     pass
