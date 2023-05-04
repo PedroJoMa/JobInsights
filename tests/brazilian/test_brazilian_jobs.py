d3eaf5bbe254eec1f0c9f92ee1e3bd13092056e1
PedroJoMa <pejomadejefi@gmail.com>
2023-05-04 10:35:29 -0300

    Implementa testes para a função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+mock = {'title': 'Maquinista', 'salary': '2000', 'type': 'trainee'}
 
 
 def test_brazilian_jobs():
+    path = "tests/mocks/brazilians_jobs.csv"
+    assert read_brazilian_file(path)[0] == mock
     pass
